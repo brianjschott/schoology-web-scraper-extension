bef9dd8075c58329e9e6ee8410ca304209ce6b9b
Brian Schott <brianjschott@gmail.com>
2022-11-11 13:37:36 -0500

    Accessing all elements correctly, now to handle iterating thru lists

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -6,10 +6,12 @@ console.log("Scraper file loaded")
 console.log("Scraping rubric...")
 const schoologyAssignmentPage = "https://classroom.cgps.org/assignment"
 // `tab` will either be a `tabs.Tab` instance or `undefined`.
-const rubricRowName = document.querySelector(`tr.rubric-row.ng-scope th.rubric-row-title`).innerHTML
-const rubricRowScore = document.querySelector(`.right-column > .rubric-table > :nth-child(2) td.rating-wrapper.ng-scope.selected div.rating-item`).innerHTML
-const totalScore = document.querySelector(`div.s-slider.s-js-manage-focus.rubric-grades-edit.ng-scope.opened div.bottom`).innerHTML
-console.log(rubricRowName, rubricRowScore, totalScore)
+const rubricRowName = document.querySelectorAll(`tr.rubric-row.ng-scope th.rubric-row-title`)
+const rubricRowScore = document.querySelectorAll(`.right-column > .rubric-table > :nth-child(2) td.rating-wrapper.ng-scope.selected div.rating-item`)
+const totalScore = document.querySelector(`div.s-slider.s-js-manage-focus.rubric-grades-edit.ng-scope.opened .assigned-grades-total span`).innerHTML
+console.log(rubricRowName[0])
+console.log(rubricRowScore[0])
+console.log(totalScore)
     
 
 
