c84a92d82f9da3d3d1dc2adf25d0215fa192650a
Brian Schott <brianjschott@gmail.com>
2022-11-13 22:09:13 -0500

    Scraper successfully compiling JSON objects of rubric data!

--- scripts/background.js
+++ scripts/background.js
@@ -6,10 +6,24 @@ chrome.action.onClicked.addListener((tab) => {
   });
 });
 
-  chrome.runtime.onMessage.addListener((msg, sender) => {
-    // First, validate the message's structure.
-    if ((msg.from === 'content') && (msg.subject === 'showPageAction')) {
-      // Enable the page-action for the requesting tab.
-      chrome.pageAction.show(sender.tab.id);
-    }
+chrome.runtime.onMessage.addListener((msg, sender) => {
+  console.log("Message heard")
+  if (msg.rubricSaved) {
+    //Pulled from stackexchange post, basically downloads data as a way to save it locally
+    chrome.storage.local.get(null, function(items) { // null implies all items
+      // Convert object to a string.
+      var result = JSON.stringify(items);
+      
+      // Save as file
+      var url = 'data:application/json;base64,' + btoa(result);
+      chrome.downloads.download({
+          url: url,
+          filename: `${msg.studentName}-${msg.assignmentNumber}.txt` //update to student name
+      }, (downloadID) => { //callback when download started not finished
+          if (downloadID) { //if error starting download, downloadID is undefined
+              chrome.storage.local.clear(); //empties for next rubric to be downloaded
+          }
+      });
   });
+  }
+});
--- scripts/scrape.js
+++ scripts/scrape.js
@@ -5,9 +5,9 @@ console.log("Scraper file loaded")
 const rubricRowNameList = document.querySelectorAll(`tr.rubric-row.ng-scope th.rubric-row-title`)
 const rubricRowScoreList = document.querySelectorAll(`.right-column > .rubric-table > :nth-child(2) td.rating-wrapper.ng-scope.selected div.rating-item`)
 const totalScore = document.querySelector(`div.s-slider.s-js-manage-focus.rubric-grades-edit.ng-scope.opened .assigned-grades-total span`).innerHTML
-const studentName = document.querySelector(`.enrollment-dd-name`)
+const studentName = document.querySelector(`.enrollment-dd-name`).textContent
 const assignmentNumber = window.location.pathname.substring(window.location.pathname.indexOf("assignment/")+11,window.location.pathname.lastIndexOf('/'));
-
+console.log(studentName)
 let rubric = []
 //for (item of rubricRowNameList) 
 for (let i = 0; i < rubricRowNameList.length; i++)
@@ -28,26 +28,10 @@ for (let i = 0; i < rubricRowNameList.length; i++)
 }
 
 chrome.storage.local.set({rubric: rubric}, function() {
-    downloadData()
+    console.log("Saving to local storage")
+    chrome.runtime.sendMessage({rubricSaved: true, studentName: studentName, assignmentNumber: assignmentNumber})
 }) //add object formed above here 
 
 
-function downloadData() {
-    //Pulled from stackexchange post, basically downloads data as a way to save it locally
-    chrome.storage.local.get(null, function(items) { // null implies all items
-        // Convert object to a string.
-        var result = JSON.stringify(items);
-        console.log(result)
-        // Save as file
-        var url = 'data:application/json;base64,' + btoa(result);
-        chrome.downloads.download({
-            url: url,
-            filename: `${studentName}-${assignmentNumber}` //update to student name
-        }, (downloadID) => { //callback when download started not finished
-            if (downloadID) { //if error starting download, downloadID is undefined
-                chrome.storage.local.clear(); //empties for next rubric to be downloaded
-            }
-        });
-    });
-}
+
 
